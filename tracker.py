c6d2920152468a11f684c83523ea17145e810b9c
rafficer <rafficer@ncrypt.sh>
2018-04-15 22:24:44 +0200

    Support for running multiple times a day

--- tracker.py
+++ tracker.py
@@ -6,10 +6,30 @@ import sys
 
 subreddits = sys.argv
 del subreddits[0]
+date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-def get_subcount(subreddit):
+def get_lastdate(file):
+        lastline = get_lastline(file)
+        array = lastline.split(",", 1)
+        date = array[0]
+        return date
+
+def get_lastline(file):
+    with open(file, "r") as f1:
+        lastline = f1.readlines()[-1]
+        return lastline
+
+def process(subreddit):
     filename = "subcounts.{}.csv".format(subreddit)
+    lastdate = get_lastdate(filename)
 
+    if date in lastdate:
+        exit(0)
+    else:
+        get_subcount(subreddit, filename)
+
+
+def get_subcount(subreddit, filename):
     request_headers = {
     "Accept-Language": "en-US,en;q=0.5",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64; rv:40.0) Gecko/20100101 Firefox/40.0",
@@ -24,7 +44,6 @@ def get_subcount(subreddit):
     jdata = json.loads(res.read())
 
     subscribers = jdata['data']['subscribers']
-    date = datetime.datetime.now().strftime("%Y-%m-%d")
 
     if os.path.isfile(filename) == False:
         with open(filename,"w+") as csvfile:
@@ -32,7 +51,7 @@ def get_subcount(subreddit):
                 csvfile.write("Date, Subcount\n")
 
     with open(filename, "a") as csvfile:
-        csvfile.write("{},{}\n".format(date, subscribers))
+        csvfile.write("\n{},{}".format(date, subscribers))
 
 for sub in subreddits:
-    get_subcount(sub)
+    process(sub)
