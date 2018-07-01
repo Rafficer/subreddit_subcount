3487eccafd38172c61a7d2e7190c7050f3b70afe
rafficer <rafficer@ncrypt.sh>
2018-07-01 22:41:19 +0200

    Update to Database

---
+++ transferCSVtoDB.py
@@ -0,0 +1,28 @@
+import MySQLdb
+
+filename = "subcounts.subredditname.csv"
+subredditname = "subredditname"
+
+conn = MySQLdb.connect(host="DBServerHostname",
+        user="DBUser",
+        passwd="DBPassword",
+        db="DBName")
+
+dbc = conn.cursor()
+
+
+with open(filename, 'r') as myfile:
+  data = myfile.readlines()
+
+filelength = len(data)
+i=0
+for x in data:
+    if(len(x) != 1):
+            if(i != 0):
+                delimited = x.split(",")
+                date = delimited[0]
+                subcount = delimited[1]
+                dbc.execute("""INSERT INTO Subcounts (Date, Subreddit, Subcount) VALUES (%s,%s,%s)""",(date,subredditname,subcount))
+                conn.commit()
+                conn.close()
+            i=i+1
